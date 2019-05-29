e905be0757dc927f317ef39218d5110ad47f26d7
Halie <halie.creollo@gmail.com>
2019-05-29 18:44:50 -0500

    route typos

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ var PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-var apiRoutes = require('./app/routing/api-routes.js');
-var htmlRoutes = require('./app/routing/html-routes.js');
+var apiRoutes = require("./app/routing/apiRoutes");
+var htmlRoutes = require("./app/routing/htmlRoutes.js");
 
 //routes
 apiRoutes(app);
